aa8245740cbbd4169f2030f023fe4812c63bdbab
levifreire7 <levifreire7@gmail.com>
2023-08-04 18:37:35 -0300

    templates

---
+++ recipes/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html', {
+        'name': 'Levi Freire'
+    })
+
+
+def contato(request):
+    return render(request, 'contato.html')
+
+
+def sobre(request):
+    return HttpResponse("Sobre")
